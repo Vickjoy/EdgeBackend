0a313553c1c4136d8286abe38123eb52e92d0219
Vickjoy <joyvicky2003@gmail.com>
2025-07-18 10:07:34 +0300

    Product detail page & breadcrumb navigation

--- Systems/serializers.py
+++ Systems/serializers.py
@@ -6,23 +6,60 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['id', 'name', 'type', 'slug']
 
+class CategoryMiniSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'slug']
+
 class SubcategorySerializer(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = Subcategory
         fields = ['id', 'name', 'slug', 'category']
 
+class SubcategoryMiniSerializer(serializers.ModelSerializer):
+    category = CategoryMiniSerializer()
+    class Meta:
+        model = Subcategory
+        fields = ['id', 'name', 'slug', 'category']
+
 class ProductSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(required=False, allow_null=True)
+    subcategory = SubcategoryMiniSerializer()
+    subcategory_slug = serializers.SerializerMethodField()
+    category_slug = serializers.SerializerMethodField()
+    documentation_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price', 'description', 'image', 'features', 'specifications', 'documentation', 'status', 'subcategory', 'slug']
+        fields = [
+            'id', 'name', 'price', 'description', 'image', 'features', 'specifications',
+            'documentation', 'documentation_url', 'status', 'subcategory', 'subcategory_slug', 'category_slug', 'slug'
+        ]
         extra_kwargs = {
             'name': {'required': True},
             'price': {'required': True},
             'subcategory': {'required': True},
         }
+
+    def get_subcategory_slug(self, obj):
+        return obj.subcategory.slug if obj.subcategory else None
+
+    def get_category_slug(self, obj):
+        return obj.subcategory.category.slug if obj.subcategory and obj.subcategory.category else None
+
+    def get_documentation_url(self, obj):
+        doc = obj.documentation
+        request = self.context.get('request')
+        if doc:
+            # If it looks like a URL, return as is
+            if doc.startswith('http://') or doc.startswith('https://'):
+                return doc
+            # If it looks like a file path, build full URL
+            if request is not None:
+                return request.build_absolute_uri('/media/' + doc.lstrip('/'))
+            return '/media/' + doc.lstrip('/')
+        return None
 
     def to_internal_value(self, data):
         subcategory_value = data.get('subcategory')
